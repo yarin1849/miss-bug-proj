6708411bd4a56c3a656ff53ef7cab3b0f7c1a15c
yarin1849 <135698206+yarin1849@users.noreply.github.com>
2025-01-27 19:02:07 +0200

    Add files via upload

---
+++ public/pages/BugDetails.jsx
@@ -0,0 +1,33 @@
+const { useState, useEffect } = React
+const { Link, useParams } = ReactRouterDOM
+
+import { bugService } from '../services/bug.service.local.js'
+import { showErrorMsg } from '../services/event-bus.service.js'
+
+export function BugDetails() {
+
+    const [bug, setBug] = useState(null)
+    const { bugId } = useParams()
+
+    useEffect(() => {
+        bugService.getById(bugId)
+            .then(bug => setBug(bug))
+            .catch(err => showErrorMsg(`Cannot load bug`, err))
+    }, [])
+
+    return <div className="bug-details">
+        <h3>Bug Details</h3>
+        {!bug && <p className="loading">Loading....</p>}
+        {
+            bug &&
+            <div>
+                <h4>{bug.title}</h4>
+                <h5>Severity: <span>{bug.severity}</span></h5>
+                <p>{bug.description}</p>
+            </div>
+        }
+        <hr />
+        <Link to="/bug">Back to List</Link>
+    </div>
+
+}
