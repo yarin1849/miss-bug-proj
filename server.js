5bb611d09379c933cb5603571fb4daa9bab4b3eb
Yarin Bracha <yarin1849@gmail.com>
2025-01-27 17:50:31 +0200

    Add CRUDL API

--- server.js
+++ server.js
@@ -1,4 +1,53 @@
 import express from 'express'
+import { bugService } from './services/bug.service.js'
+
 const app = express()
 app.get('/', (req, res) => res.send('Hello there'))
 app.listen(3030, () => console.log('Server ready at port 3030'))
+
+app.get('/api/bug', (req, res) => {
+
+    bugService.query()
+        .then(bugs => res.send(bugs))
+        .catch(err => {
+            loggerService.error('Cannot get bugs', err)
+            res.status(500).send('Cannot get bugs')
+        })
+})
+
+app.get('/api/bug/save', (req, res) => {
+    const bugToSave = {
+        _id: req.query._id,
+        title: req.query.title,
+        description: req.query.description,
+        severity: +req.query.severity || 3,
+        createdAt: req.query.createdAt || Date.now(),
+    }
+
+    bugService.save(bugToSave)
+        .then(savedCar => res.send(savedCar))
+        .catch(err => {
+            loggerService.error('Cannot save bug:', err)
+            res.status(500).send('Cannot save bug')
+        })
+})
+
+app.get('/api/bug/:bugId', (req, res) => {
+    const { bugId } = req.params
+    bugService.getById(bugId)
+        .then(bug => res.send(bug))
+        .catch(err => {
+            loggerService.error('Cannot get bug:', err)
+            res.status(500).send('Cannot get bug')
+        })
+})
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const { bugId } = req.params
+    bugService.remove(bugId)
+        .then(() => res.send(bugId + ' Removed Successfully!'))
+        .catch(err => {
+            loggerService.error('Cannot remove bug:', err)
+            res.status(500).send('Cannot remove bug')
+        })
+})
